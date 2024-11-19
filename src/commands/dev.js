71eacf248088840e291f98e1f75f0fac631837e7
rmenner <ryan.menner@alaskaair.com>
2024-11-19 18:23:57 -0500

    perf: refactor dev cli options to allow for multiple instances #5

--- src/commands/dev.js
+++ src/commands/dev.js
@@ -4,10 +4,12 @@ import { shell } from "../utils/shell.js";
 export default program
   .command("dev")
   .description("Runs web-dev-server command")
-  .option("-p, --path <type>", "Customize the path")
+  .option("-o, --open <type>", "Open server to a specific directory")
+  .option("-p, --port <type>", "Change the server port")
   .action((options) => {
     shell("npx web-dev-server", [
-      `--open ${options.path || "demo/"}`,
+      `--open ${options.open || "demo/"}`,
+      `--port ${options.port || "8000"}`,
       "--node-resolve",
       "--watch",
     ]);
