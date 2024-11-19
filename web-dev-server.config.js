9f9cd759d27e83db02ae58b80841ae3297a75a28
rmenner <ryan.menner@alaskaair.com>
2024-11-19 18:23:57 -0500

    perf: refactor dev cli options to allow for multiple instances #5

--- web-dev-server.config.js
+++ web-dev-server.config.js
@@ -3,7 +3,7 @@ import { hmrPlugin, presets } from "@open-wc/dev-server-hmr";
 export default {
   plugins: [
     hmrPlugin({
-      include: ["src/**/*", "apiExamples/**/*"],
+      include: ["src/**/*", "demo/**/*", "apiExamples/**/*", "docs/**/*"],
       // both v3 & v2
       presets: [presets.lit, presets.litElement],
     }),
