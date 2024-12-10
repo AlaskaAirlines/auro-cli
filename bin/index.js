7ff5004fb37ef7c539efc363dbc476f7250179e4
rmenner <ryan.menner@alaskaair.com>
2024-12-10 17:07:39 -0500

    feat: migration comannd and script created #22

--- bin/index.js
+++ bin/index.js
@@ -4,11 +4,17 @@ import { program } from "commander";
 
 import auroSplash from "../src/utils/auro-splash.js";
 import devCommand from "../src/commands/dev.js";
+import migrateCommand from "../src/commands/migrate.js";
 
 auroSplash();
 
 program.name("auro-cli").version("0.0.1").description("Auro CLI");
 
-devCommand();
+const commands = () => {
+  devCommand();
+  migrateCommand();
+};
+
+commands();
 
 program.parse();
