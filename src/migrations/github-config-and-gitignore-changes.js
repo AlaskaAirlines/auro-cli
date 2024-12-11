8e9f78e81d72ac898bc9ef2fcad6916d73923cc9
rmenner <ryan.menner@alaskaair.com>
2024-12-11 17:30:04 -0500

    perf: new line, commenting, and checking for existing patterns #22

--- src/migrations/github-config-and-gitignore-changes.js
+++ src/migrations/github-config-and-gitignore-changes.js
@@ -3,22 +3,55 @@ import { promisify } from "util";
 import simpleGit from "simple-git";
 import { Logger } from "@aurodesignsystem/auro-library/scripts/utils/logger.mjs";
 
-import "../commands/sync.js";
-
 const appendFile = promisify(fs.appendFile);
+const readFile = promisify(fs.readFile);
 const git = simpleGit();
 
-// Function to add file to .gitignore
-const addToGitignore = async (pattern) => {
+/**
+ * Function to check if pattern exists in .gitignore.
+ * @async
+ * @param {string} pattern .gitignore pattern.
+ * @returns {boolean}
+ */
+const checkGitignore = async (pattern) => {
+  if (pattern === "") return false;
   try {
-    await appendFile(".gitignore", `\n${pattern}`);
-    Logger.success(`${pattern} added to .gitignore`);
+    const fileContent = await readFile(".gitignore", "utf-8");
+    return fileContent.includes(pattern);
   } catch (err) {
-    Logger.error(err);
+    Logger.error("Error reading file:", err);
+    return false;
   }
 };
 
-// Function to remove file from git cache
+/**
+ * Function to add pattern to .gitignore.
+ * @async
+ * @param {String} pattern .gitignore pattern.
+ * @param {Boolean} log If success message should be shown.
+ */
+const addToGitignore = async (pattern, log = true) => {
+  await checkGitignore(pattern).then(async (result) => {
+    if (result) {
+      Logger.warn(`${pattern} already exists`);
+    } else {
+      try {
+        await appendFile(".gitignore", `\n${pattern}`);
+        if (log) {
+          Logger.success(`${pattern} added to .gitignore`);
+        }
+      } catch (err) {
+        Logger.error(err);
+      }
+    }
+  });
+};
+
+/**
+ * Function to remove file from git cache.
+ * @async
+ * @param {Array} files Array of files to remove from git cache.
+ */
 const removeFromGitCache = async (files) => {
   try {
     await git.rmKeepLocal(files);
@@ -30,8 +63,13 @@ const removeFromGitCache = async (files) => {
 
 // Run tasks sequentially
 const run = async () => {
+  await addToGitignore(
+    "## Added by Auro CLI migration (github-config-and-gitignore-changes)",
+    false,
+  );
   await addToGitignore("demo/*.md");
-  await addToGitignore("demo/*min.js");
+  await addToGitignore("demo/*.min.js");
+  await addToGitignore("", false);
   await removeFromGitCache([
     "demo/api.md",
     "demo/index.md",
