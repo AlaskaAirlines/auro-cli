9649571ff3d634e89e86dbf063c5a58e9e9f31a6
Doug <doug@dougflynn.dev>
2024-12-10 19:46:48 -0800

    feat: add TypeScript support

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+import { program } from "commander";
+
+// @ts-ignore
+import auroSplash from "./utils/auro-splash.js";
+
+// Register commands (importing them will register them)
+import "./commands/dev.js";
+import "./commands/migrate.js";
+import "./commands/sync.js";
+
+auroSplash();
+
+program.name("auro-cli").version("0.0.1").description("Auro CLI");
+
+program.parse();
