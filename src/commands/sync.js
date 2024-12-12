42b3f47ba46156935a8b5bcbfc23f3a92ee2e4a0
Doug <doug@dougflynn.dev>
2024-12-11 22:56:20 -0800

    fix: add codeowners newline fix
    something about the formatting is strange here... it shows up with multiple newlines when viewing in a local copy, but not when merged in and pushed to github? might not really be required but idk.

--- src/commands/sync.js
+++ src/commands/sync.js
@@ -3,6 +3,7 @@ import process from "node:process";
 
 import { Logger } from "@aurodesignsystem/auro-library/scripts/utils/logger.mjs";
 import { syncDotGithubDir } from "../scripts/syncDotGithubDir.js";
+import { readFile, writeFile } from "node:fs/promises";
 
 export default program
   .command("sync")
@@ -19,4 +20,24 @@ export default program
     const cwd = process.cwd();
 
     await syncDotGithubDir(cwd);
+
+    // Cleanup for specific files
+    // ------------------------------------------------------
+
+    // Some files have specific cleanup tasks that need to be run after syncing
+
+    // CODEOWNERS - has a bizarre issue with line endings. This is a workaround!
+    // Maybe it has to do with the file type since there's no ending?
+    const codeownersPath = `${cwd}/.github/CODEOWNERS`;
+    const codeowners = await readFile(codeownersPath, { encoding: "utf-8" });
+
+    // Convert line endings to \n
+    const codeownersFixed = codeowners
+      .replace(/\r\n/gu, "\n")
+      .replace(/\n\n/gu, "\n");
+    await writeFile(codeownersPath, codeownersFixed, { encoding: "utf-8" });
+
+    if (codeownersFixed.includes("\r") || codeownersFixed.includes("\n\n")) {
+      Logger.error("CODEOWNERS file still has Windows line endings.");
+    }
   });
