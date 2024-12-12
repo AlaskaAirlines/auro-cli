3b921d597f17f1fe7d07d3fe0c92dedcbced26dc
rmenner <ryan.menner@alaskaair.com>
2024-12-11 23:55:53 -0500

    feat: remove git branch and commit commands and add the extra gitignore stuff

--- src/migrations/github-config-and-gitignore-changes.js
+++ src/migrations/github-config-and-gitignore-changes.js
@@ -2,30 +2,21 @@ import { Logger } from "@aurodesignsystem/auro-library/scripts/utils/logger.mjs"
 
 import { Git } from "../utils/gitUtils.js";
 
-const commitMessage = `chore: update .gitignore and remove demo files from cache
-
-This commit removes the demo files from the cache and updates the
-.gitignore file to exclude the demo files from being tracked by git.
-
-We were getting a ton of noise in the repo from the demo files on
-every build. This commit should help clean that up.
-
-`;
-
 // Run tasks sequentially
 const run = async () => {
-  await Git.createBranch("migration/git-config-updates");
-
+  await Git.addToGitignore(
+    "## Added by Auro CLI migration (github-config-and-gitignore-changes)",
+    false,
+  );
   await Git.addToGitignore("demo/*.md");
-  await Git.addToGitignore("demo/*min.js");
+  await Git.addToGitignore("demo/*.min.js");
+  await Git.addToGitignore("", false);
   await Git.removeFromGitCache([
     "demo/api.md",
     "demo/index.md",
     "demo/index.min.js",
     "demo/api.min.js",
   ]);
-
-  await Git.commitStagedFiles(commitMessage);
 };
 
 run().catch(Logger.error);
