a039829b384cc285ffba735adef26f8c17b7124a
Jason Baker <jason@capsule42.com>
2024-12-20 13:02:39 -0800

    feat: add script for updating node version #26

---
+++ src/migrations/package-update-node/migration.js
@@ -0,0 +1,32 @@
+import { Logger } from "@aurodesignsystem/auro-library/scripts/utils/logger.mjs";
+
+import fs from "fs";
+// import { path } from "path";
+
+// Run tasks sequentially
+const run = () => {
+  try {
+    const version = "^20 || ^22";
+    const packageJsonPath = "package.json";
+
+    // Read package.json
+    const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
+
+    packageJson.engines.node = version;
+
+    // Write the updated package.json back to the file
+    fs.writeFileSync(
+      packageJsonPath,
+      `${JSON.stringify(packageJson, null, 2)}\n`, // eslint-disable-line no-magic-numbers
+      "utf8",
+    );
+  } catch (error) {
+    Logger.error(
+      `Failed to update node engine version in package.json: ${error}`,
+    );
+  }
+
+  Logger.success(`Node engine version updated to version in package.json`);
+};
+
+run();
