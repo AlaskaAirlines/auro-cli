a5d9792d76d77e04a8e4441984fa620fa0cea62f
rmenner <ryan.menner@alaskaair.com>
2025-11-17 14:59:25 -0500

    fix: correct glob pattern in custom elements manifest configuration #232

--- src/configs/custom-elements-manifest.config.mjs
+++ src/configs/custom-elements-manifest.config.mjs
@@ -1,7 +1,7 @@
 import { cemSorterPlugin } from "@wc-toolkit/cem-sorter";
 
 export default {
-  globs: ["src/*.*js", "scripts/wca/**/*.*js"],
+  globs: ["src/**/*.*js", "scripts/wca/**/*.*js"],
   litelement: true,
   packagejson: true,
   dependencies: true,
