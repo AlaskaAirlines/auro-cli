afe5ea01006ecdb0c35c001615e30dcc84d16aaa
rmenner <ryan.menner@alaskaair.com>
2025-12-10 00:16:34 -0500

    refactor: addDtsExportsPlugin to it's own file and add ts support

--- src/configs/custom-elements-manifest.config.mjs
+++ src/configs/custom-elements-manifest.config.mjs
@@ -1,51 +1,6 @@
 import { cemSorterPlugin } from "@wc-toolkit/cem-sorter";
 import { jsxTypesPlugin } from "@wc-toolkit/jsx-types";
-import fs from "fs/promises";
-
-function addDtsExportsPlugin() {
-  return {
-    // Make sure to always give your plugin a name! This helps when debugging
-    name: 'add-dts-exports-plugin',
-    packageLinkPhase({customElementsManifest}){
-
-      // find modules where path matches 'src/index.js'
-      const exportedModules = customElementsManifest.modules.filter(
-        (mod) => mod.path.endsWith("src/index.js")
-      );
-
-      const exportNames = [];
-
-      // collect all export names
-      exportedModules[0].exports.forEach((exp) => {
-        exportNames.push(exp.name);
-      });
-
-      if (exportNames.length === 0) {
-        console.warn(
-          "No exports found for 'src/index.js'. Skipping export statement addition."
-        );
-        return;
-      }
-
-      // construct export statement
-      const exportStatement = `declare global {
-  namespace svelteHTML {
-    interface IntrinsicElements extends CustomElements {}
-  } 
-}\n
-
-export { ${exportNames.join(', ')} } from "./index.js";\n`;
-
-      // append export statement to dist/index.d.ts
-      fs.appendFile('dist/index.d.ts', exportStatement).then(() => {
-        console.info('Appended export statements to index.d.ts');
-      }).catch((err) => {
-        console.error(`Error appending to index.d.ts: ${err.message}`);
-      });
-
-    },
-  }
-}
+import addDtsExportsPlugin from "#scripts/docs/plugins/addDtsExportsPlugin";
 
 export default {
   globs: ["src/**/*.*js", "scripts/wca/**/*.*js"],
@@ -60,8 +15,8 @@ export default {
     jsxTypesPlugin({
       fileName: "index.d.ts",
       outdir: "dist",
-      excludeCssCustomProperties: true
+      excludeCssCustomProperties: true,
     }),
-    addDtsExportsPlugin(),
+    addDtsExportsPlugin()
   ],
 };
