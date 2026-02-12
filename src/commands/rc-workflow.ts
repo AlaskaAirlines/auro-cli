c1467b70d419ed676a48fb9465e869c49439f255
rmenner <ryan.menner@alaskaair.com>
2026-02-12 14:58:20 -0500

    chore: remove repository option from rc-workflow command

--- src/commands/rc-workflow.ts
+++ src/commands/rc-workflow.ts
@@ -5,12 +5,9 @@ import { RCWorkflow } from "#scripts/rc-workflow/index.ts";
 export default program
   .command("rc-workflow")
   .description("Generate RC issue and pull request")
-  .option("-r, --repo <repository>", "Specify the repository", "")
-  .action(async (option) => {
+  .action(async () => {
 
   const workflow = await RCWorkflow.create();
   await workflow.createReleaseCandidate();
-
-  console.log(option.repo)
 });
 
